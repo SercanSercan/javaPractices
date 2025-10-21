8f926ddcb7344b7a8955d42d40f6a0ae54f3d26f
Sercan Leylek <sercan.leylek@fremtind.no>
2025-10-21 17:05:58 +0200

    Add challenge project

---
+++ Java Brains Challenges/JB_Challenges/src/Main.java
@@ -0,0 +1,10 @@
+import com.javabrains.challenges.C1_FizzBuzz;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        C1_FizzBuzz fizzBuzz = new C1_FizzBuzz();
+        fizzBuzz.execute();
+    }
+}
---
+++ Java Brains Challenges/JB_Challenges/src/com/javabrains/challenges/C1_FizzBuzz.java
@@ -0,0 +1,18 @@
+package com.javabrains.challenges;
+
+public class C1_FizzBuzz {
+
+    public static void execute() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
